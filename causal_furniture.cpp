8b9ece2f145163c126a8411f52cd2ae047bfabef
Kingsley024 <kaishuo_zhang@brown.edu>
2023-02-28 21:48:34 -0500

    Update causal_furniture.cpp
    wall lamp with perfect model

--- causal_furniture.cpp
+++ causal_furniture.cpp
@@ -486,7 +486,7 @@ bool CAUSAL_FURNITURE::isValid(std::pair<std::string, std::string> connection) c
 
 bool CAUSAL_FURNITURE::isCausal(std::pair<std::string, std::string> connection) const
 {
-    std::vector<std::pair<std::string, std::string>> validconnections{ std::make_pair("wall plug", "backplate"),std::make_pair("wall plug", "lightbulb"), std::make_pair("backplate","lightbulb"), std::make_pair("lightbulb","lamp body") };
+    std::vector<std::pair<std::string, std::string>> validconnections{ std::make_pair("wall plug", "backplate"),std::make_pair("lightbulb", "backplate"), std::make_pair("lightbulb","lamp body") };
     auto it3 = std::find_if(validconnections.begin(), validconnections.end(), [&](std::pair<std::string, std::string> obj) { return ((obj.first == connection.first) && (obj.second == connection.second)); });
     auto it4 = std::find_if(validconnections.begin(), validconnections.end(), [&](std::pair<std::string, std::string> obj) { return ((obj.first == connection.second) && (obj.second == connection.first)); });
     
