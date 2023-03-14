5371d8f7dc84517bae5cfbfaa64327907f47dfef
taoburga <francisco_burga_montoya@brown.edu>
2023-03-14 13:32:55 -0400

    Update causal_furniture.cpp
    kerosene lamp User 11 (recoded to User 4)

--- causal_furniture.cpp
+++ causal_furniture.cpp
@@ -486,7 +486,7 @@ bool CAUSAL_FURNITURE::isValid(std::pair<std::string, std::string> connection) c
 
 bool CAUSAL_FURNITURE::isCausal(std::pair<std::string, std::string> connection) const
 {
-    std::vector<std::pair<std::string, std::string>> validconnections{ std::make_pair("kerosene", "lighter"), std::make_pair("lighter", "wick"),std::make_pair("fuel tank", "wick"),std::make_pair("burner", "wick"), std::make_pair("fuel tank", "lighter"),std::make_pair("fuel tank", "burner"), std::make_pair("lighter", "burner"), std::make_pair("wick", "chimney")};
+    std::vector<std::pair<std::string, std::string>> validconnections{ std::make_pair("fuel tank", "lighter"), std::make_pair("kerosene", "lighter"),std::make_pair("burner", "lighter"),std::make_pair("fuel tank", "kerosene"), std::make_pair("fuel tank", "burner"),std::make_pair("kerosene", "burner"), std::make_pair("lighter", "chimney"), std::make_pair("lighter", "wick"), std::make_pair("chimney", "wick")};
     auto it3 = std::find_if(validconnections.begin(), validconnections.end(), [&](std::pair<std::string, std::string> obj) { return ((obj.first == connection.first) && (obj.second == connection.second)); });
     auto it4 = std::find_if(validconnections.begin(), validconnections.end(), [&](std::pair<std::string, std::string> obj) { return ((obj.first == connection.second) && (obj.second == connection.first)); });
     
