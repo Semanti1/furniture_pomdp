f10158607081cfc65fef6aea9026afafd5d4c340
taoburga <francisco_burga_montoya@brown.edu>
2023-03-16 23:24:42 -0400

    Update causal_furniture.cpp
    wall lamp, User 9 (recoded to User 2). Note: flat causal model!

--- causal_furniture.cpp
+++ causal_furniture.cpp
@@ -486,7 +486,7 @@ bool CAUSAL_FURNITURE::isValid(std::pair<std::string, std::string> connection) c
 
 bool CAUSAL_FURNITURE::isCausal(std::pair<std::string, std::string> connection) const
 {
-    std::vector<std::pair<std::string, std::string>> validconnections{ std::make_pair("wall plug", "backplate"), std::make_pair("lightbulb", "wall plug")};
+    std::vector<std::pair<std::string, std::string>> validconnections{ std::make_pair("wall plug", "backplate"), std::make_pair("lightbulb", "wall plug"), std::make_pair("lamp body", "wall plug"), std::make_pair("lamp body", "lightbulb"), std::make_pair("lightbulb", "backplate"), std::make_pair("lamp body", "backplate")};
     auto it3 = std::find_if(validconnections.begin(), validconnections.end(), [&](std::pair<std::string, std::string> obj) { return ((obj.first == connection.first) && (obj.second == connection.second)); });
     auto it4 = std::find_if(validconnections.begin(), validconnections.end(), [&](std::pair<std::string, std::string> obj) { return ((obj.first == connection.second) && (obj.second == connection.first)); });
     
